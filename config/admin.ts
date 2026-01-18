f38820f87bde444ac727c69dc16a5b44e2da49e0
Amine Benjelloun <AmineBenjelloun@MacBook-Pro-de-Amine-2.local>
2026-01-18 14:40:42 +0100

    chore: finalize Strapi v5 security (api token salt)

--- config/admin.ts
+++ config/admin.ts
@@ -5,16 +5,4 @@ export default ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
 });
